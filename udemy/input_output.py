c242dece509b16d869e374fa7c5e78cd7af65375
Inderpal Singh <inderpal2406@gmail.com>
2020-03-22 23:39:44 +0530

    Add input_output.py

---
+++ udemy/input_output.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+'''
+a=2
+b=7
+sum=a+b
+print(f"Sum of {a} and {b} is {sum}.")
+'''
+
+'''
+a=input("Enter value of a: ")
+b=input("Enter value of b: ")
+print(f"The value of variable a is {a} and its type is {type(a)}.")
+print(f"The value of variable b is {b} and its type is {type(b)}.")
+'''
+
+'''
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+print(f"The value of variable a is {a} and its type is {type(a)}.")
+print(f"The value of variable b is {b} and its type is {type(b)}.")
+'''
+
+a=eval(input("Enter tha value of a: "))
+b=eval(input("Enter the value of b: "))
+print(f"The value of variable a is {a} and its data type is {type(a)}.")
+print(f"The value of variable b is {b} and its data type is {type(b)}.")
