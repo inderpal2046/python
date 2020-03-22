66c84c7ac2572e15c4d402d1f5aa36579dca9037
Inderpal Singh <inderpal2406@gmail.com>
2020-03-22 11:52:46 +0530

    Add datatypes.py

---
+++ udemy/datatypes.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+#x=2
+#y=3.14
+#z=6+7j
+#print(x)
+#print(y)
+#print(z)
+
+#x=2;y=3.14;z=7+8j
+#print(xyz)
+
+#x=2;y=3.14;z=7+8j
+#print(x y z)
+
+#x=2;y=3.14;z=7+8j
+#print(x\ty\tz)
+
+#x=2;y=3.14;z=7+8j
+#print(x \n y \n z)
+
+#x=2;y=3.14;z=7+8j
+#print(x);print(y);print(z)
+
+#x=2;y=3.14;z=7+8j
+#print(x"\n"y"\n"z)
+
+#x=2;y=3.14;z=7+8j
+#print(x + "\n" + y + "\n" + z)
+
+#x=2;y=3.14;z=7+8j
+#print(x+"\n"+y+"\n"+z)
+
+#x=2;y=3.14;z=7+8j
+#print(x,y,z)
+
+'''
+x=2
+y=3.14
+z=3+7j
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+'''
+
+'''
+inder="singh"
+name=inder
+x=2
+y=x
+print(name)
+print(y)
+'''
+
+inder="singh"
+name=inder
+print("Value, type and memory location of 'inder' variable is as follows,")
+print(inder,type(inder),id(inder))
+print("Value, type and memory location of 'name' variable is as follows,")
+print(name,type(name),id(name))
+x=2
+y=x
+z=2
+print("Value, type and memory location of 'x' variable is as follows,")
+print(x,type(x),id(x))
+print("Value, type and memory location of 'y' variable is as follows,")
+print(y,type(y),id(y))
+print("Value, type and memory location of 'z' variable is as follows,")
+print(z,type(z),id(z))
