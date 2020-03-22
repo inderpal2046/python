a7f7a3f3b58cc5ef277dd947af937d3e68c61243
Inderpal Singh <inderpal2406@gmail.com>
2020-03-22 21:05:35 +0530

    Add multivars_in_print.py

---
+++ udemy/multivars_in_print.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Defining multiple variables in single line or multiple lines
+
+#x=2;y=3;lang_name="python scripting"
+
+x=2
+y=3
+lang_name="python scripting"
+
+# Different ways of using multiple variables in print statement
+
+# way 1
+
+'''
+print(x)
+print(y)
+print(lang_name)
+'''
+
+# way 2
+
+#print(x,y,lang_name)
+
+# way 3
+
+#print("{} {} {}".format(x,y,lang_name))
+
+# way 4
+
+#print("{} \n{} \n{}".format(x,y,lang_name))
+
+# way 5
+
+#print("{} \t{} \t{}".format(lang_name,y,x))
+
+# way 6
+
+#print(f"The value of x: {x} \nThe value of y: {y} \nThe value of lang_name: {lang_name}")
+
+# way 7
+
+output=f"The value of x: {x} \nThe value of y: {y} \nThe value of lang_name: {lang_name}"
+print(output)
+
