369c7ea4b195345c181fcab8a11ea64a37a60832
Inderpal Singh <inderpal2406@gmail.com>
2020-03-25 15:32:42 +0530

    Add case_conversion.py

---
+++ udemy/case_conversion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+name="Inderpal Singh Saini"
+print(f"Original string is: {name} \n")
+print(f"length of string is: {len(name)} \n")
+lowercase=name.lower()
+print(f"Lowercase string is: {lowercase} \n")
+print(f"Uppercase string is: {name.upper()} \n")
+print(f"Below operations can be performed on our string, \n")
+print(f"{dir(name)} \n")
+print(f"The string in title format is: {name.title()} \n")
+print(f"The string in capitalized format is: {name.capitalize()} \n")
+print(f"The strign in swapped case format is: {name.swapcase()} \n")
+#print(f"The string in translate case format is: {name.translate()} \n")
