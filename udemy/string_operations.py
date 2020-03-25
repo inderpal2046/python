6b02ad7c21a78e9cb2121afbdb6d2b8615a6ae22
Inderpal Singh <inderpal2406@gmail.com>
2020-03-25 13:59:41 +0530

    Update string_operations.py

--- udemy/string_operations.py
+++ udemy/string_operations.py
@@ -48,8 +48,68 @@ print(f"memory location of string_3 variable is: {id(string_3)}")
 
 # To print specific characters of the string 
 
+'''
 name="Inder"
+print("Below are positive index values serially,")
 print(name)
 print(name[0])
 print(name[1])
+print(name[2])
+print(name[3])
 print(name[4])
+print()
+print("Below are negative index values serially,")
+print(name)
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+'''
+
+# Print a range of characters.
+
+'''
+name="Inderpal"
+print(name)
+print(name[0:5])	# Start print from 0 index and print 5 characters
+print(name[0:])		# Start print from 0 index and print till last character
+nickname=name[0:5]	# save output string
+print(nickname)		# print output string
+print(name[:5])		# start from index 0 and go upto one index less than the mentioned index 5
+print(name[2:5])	# start from index 2 and go upto one index less than the mentioned index 5
+'''
+
+# Modifying string characters
+
+'''
+name="Inderpal"
+#name[0]="U"		# error, string object does not support item assignment
+#del name[3]		# error
+print(name)
+del name		# success
+print(name)		# error
+'''
+
+# Length of string
+
+'''
+name="Inderpal Singh Saini"
+length=len(name)
+print(f"Length of the string is: {length}")
+print(f"Length of the string is: {len(name)}")
+'''
+
+# Add or concatenate 2 strings
+
+string_1="Python"
+string_2="Scripting"
+string_3=string_1+string_2
+print(string_3)
+string_4=" "
+string_5=string_1+string_4+string_2
+print(string_5)
+string_6=string_1+" "+string_2
+print(string_6)
+string_7=string_1+" "+string_2+" "+"tutorials"
+print(string_7)
