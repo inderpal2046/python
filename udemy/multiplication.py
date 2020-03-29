142880e4add356a8499110d7fcc3d9c5a8480708
Inderpal Singh <inderpal2406@gmail.com>
2020-03-29 18:12:54 +0530

    Add multiplication.py

---
+++ udemy/multiplication.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# This script will multiply 2 numbers provided as user input 
+
+import os
+os.system("clear")
+print(f"This script will multiply 2 numbers provided as user input.\n")
+a=eval(input("Enter first number : "))
+b=eval(input("Enter second number : "))
+product=a*b
+print(f"\nThe product of {a} and {b} is : {product} \n") 
