adf7f98c3a5483dee1e5f93651edd1c61ea5db7e
Inderpal Singh <inderpal2406@gmail.com>
2020-05-02 05:14:55 +0530

    Add working_with_txt_files_1.py

---
+++ udemy/working_with_txt_files_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+fo=open("name.txt","w")
+fo.write("My name is Inderpal Singh.\n")	# \n to send cursor to next line. \n plays the role of pressing Enter key.
+fo.write("My father's name is Balvinder Singh.\n")
+fo.write("My mother's name is Darshan Kaur.\n")
+fo.close()
