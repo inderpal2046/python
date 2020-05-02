ef2daeebe22f36785fd7a4769e84acef247f4988
Inderpal Singh <inderpal2406@gmail.com>
2020-05-03 01:07:22 +0530

    Add working_with_csv_files_1.py

---
+++ udemy/working_with_csv_files_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# read a csv file using readlines() function of file object (cursor position)
+
+req_file="/home/inderpal2406/tmp/data.csv"
+fo=open(req_file,"r")
+data=fo.readlines()
+fo.close()
+for i in data:
+    print(i.strip("\n"))
