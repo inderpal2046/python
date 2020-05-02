ed45c56298eff6e3d0c310546f84d7b804328bc0
Inderpal Singh <inderpal2406@gmail.com>
2020-05-02 05:54:24 +0530

    Update working_with_txt_files_6.py

--- udemy/working_with_txt_files_6.py
+++ udemy/working_with_txt_files_6.py
@@ -10,3 +10,10 @@ fo=open("friends.txt","r")
 data=fo.read()
 fo.close()
 print(data)
+
+# reading line by line
+
+fo=open("friends.txt","r")
+print(fo.readline())	# This will read only first line and print, to read and print next lines, we need to have subsequent readline()
+print(fo.readline())
+fo.close()
