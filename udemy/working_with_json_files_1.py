52868032fe6d92c8285ac326ebd1dcfc7e14d996
Inderpal Singh <inderpal2406@gmail.com>
2020-05-03 15:36:44 +0530

    Add working_with_json_files_1.py

---
+++ udemy/working_with_json_files_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# print json data using normal method of reading data.
+
+import json
+req_file="/home/inderpal2406/tmp/file_1.json"
+fo=open(req_file,"r")
+data=fo.read()
+print(data)
+fo.close()
+
+# but we want to interpret the key-value pairs and not print json file as it is.
+# check working_with_json_files_2.py
