512d37ff3c5dbc0ca633ecedcb79723546b66228
Inderpal Singh <inderpal2406@gmail.com>
2020-05-23 23:28:20 +0530

    Add script1.pyand script2.py

---
+++ udemy/script1.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+print(f"This is script1 and __name__ variable value is {__name__}")
---
+++ udemy/script2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+import script1
+print(f"This is script2 and __name__ variable value is {__name__}")
