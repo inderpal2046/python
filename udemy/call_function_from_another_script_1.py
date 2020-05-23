6e64344dd02f6dd6fa44b2dd5e4e55f833827809
Inderpal Singh <inderpal2406@gmail.com>
2020-05-23 23:27:11 +0530

    Add call_function_from_another_script_1.py

---
+++ udemy/call_function_from_another_script_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def add(a,b):
+    print(f"Sum of {a} and {b} is {a+b}")
+    return None
+
+def subtract(a,b):
+    print(f"Difference between {a} and {b} is {a-b}")
+    return None
+
+def main():
+    x=8
+    y=2
+    add(x,y)
+    subtract(x,y)
+    return None
+
+if __name__=="__main__":
+    main() 
